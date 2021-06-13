10e247ee6ae1a621133ca60309d33ab9466d10b9
IshanSrvt <76547283+IshanSrvt@users.noreply.github.com>
2021-06-13 18:21:13 +0530

    Update Windows_python_Bot.py

--- Windows_python_Bot.py
+++ Windows_python_Bot.py
@@ -1,11 +1,9 @@
-import pyttsx3    # pip install pyttsx3
 import datetime   
 
-engine = pyttsx3.init()  # object creation
 
 def welcome():  #welcome function to welcome
     print("Hello!")
-    engine.say("Hello!")
+
 
 
 hour = int(datetime.datetime.now().hour)
@@ -13,13 +11,13 @@ hour = int(datetime.datetime.now().hour)
 
 def wishme():  # WISHME FUNCTION which wishes according to time
     if hour >= 0 and hour < 12:
-        engine.say("Good Morning!")
+       
         print('Good Morning!')
     elif hour >= 12 and hour < 18:
-        engine.say("Good Afternoon!")
+     
         print("Good Afternoon!")
     else:
-        engine.say("Good evening!")
+       
         print("Good Evening!")
 
 
@@ -35,14 +33,14 @@ def main():  # main function to work and reply on keywords
     for word in user_words:
 
         if word in hi:  # response on keywords hi
-            engine.say("Hello")
+           
             print("Hello")
             return main()
 
         elif word in time:  # response on keyword time
             strTime = datetime.datetime.now().strftime("%H:%M")
             print(f"The time is {strTime}")
-            engine.say(f"The Time is {strTime}")
+       
             return main()
 
 
